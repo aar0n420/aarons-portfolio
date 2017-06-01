c1a1ee274372c8902a57d1d546641f4b0e1e8ac4
aar0n420 <15arawlinson@stjosephsrush.com>
2017-06-01 09:17:22 +0100

    Create stuff.py

---
+++ stuff.py
@@ -0,0 +1,140 @@
+print("Welcome to the history keyword logger (beta)")
+
+print("Type help if you are stuck")
+
+keyword1=input("What keyword are you looking for? ")
+
+print(keyword1)
+
+if keyword1==("primary source"):
+        print("A primary sorce is direct proof or evidence of something that comes straight from that time period")
+elif keyword1==("secondary source"):
+        print("A secondary sorce is proof or evidence of something after that time perdiod")
+elif keyword1==("research archaeology"):
+        print("Research archaeology is using books, files and artifacts to find information about something")
+elif keyword1==("artifact"):
+        print("Artifacts are things from different times. Anything around you now could be an artifact and a few centuries")
+elif keyword1==("wattle and daub"):
+        print("Wattle and daub was a construction method, used by weaving branches around wooden poles and filled with mud and soil")
+elif keyword1==("mesolithic times"):
+        print("the mesolithic times started around 7000BC and ended around 5000BC")
+elif keyword1==("bronze age"):
+        print("The bronze age started around 2000BC. People started using bronze instead of stone for weapons and tools")
+elif keyword1==("monk"):
+        print("A monk is a holy man who lives in an abbey and devotes his life to God")
+elif keyword1==("abbot"):
+	print("The Abbot is the head monk in the monastary")
+elif keyword1==("plebian"):
+	print("A plebian was a poor person who usually lived in a small home and eat grains and cheap meats")
+elif keyword1==("patricians"):
+	print("Patricians were wealthy land owners sold land to both rich and poor people")
+elif keyword1==("help"):
+	print("You can choose a definition for the following keywords: primary source, secondary source, research archaeology, wattle and daub, artifact, mesolithic times, bronze age, monk, abbot, plebian, patricians,")
+else:
+        print("I don't know that one")
+
+
+
+
+keyword1=input("Any other keywords? ")
+
+print(keyword1)
+
+if keyword1==("primary source"):
+        print("A primary sorce is direct proof or evidence of something that comes straight from that time period")
+elif keyword1==("secondary source"):
+        print("A secondary sorce is proof or evidence of something after that time perdiod")
+elif keyword1==("research archaeology"):
+        print("Research archaeology is using books, files and artifacts to find information about something")
+elif keyword1==("artifact"):
+        print("Artifacts are things from different times. Anything around you now could be an artifact and a few centuries")
+elif keyword1==("wattle and daub"):
+        print("Wattle and daub was a construction method, used by weaving branches around wooden poles and filled with mud and soil")
+elif keyword1==("mesolithic times"):
+        print("the mesolithic times started around 7000BC and ended around 5000BC")
+elif keyword1==("bronze age"):
+        print("The bronze age started around 2000BC. People started using bronze instead of stone for weapons and tools")
+elif keyword1==("monk"):
+        print("A monk is a holy man who lives in an abbey and devotes his life to God")
+elif keyword1==("abbot"):
+	print("The Abbot is the head monk in the monastary")
+elif keyword1==("plebian"):
+	print("A plebian was a poor person who usually lived in a small home and eat grains and cheap meats")
+elif keyword1==("patricians"):
+	print("Patricians were wealthy land owners sold land to both rich and poor people")
+elif keyword1==("help"):
+	print("You can choose a definition for the following keywords: primary source, secondary source, research archaeology, wattle and daub, artifact, mesolithic times, bronze age, monk, abbot, plebian, patricians,")
+else:
+        print("I don't know that one")
+
+
+
+keyword1=input("Any more? ")
+
+print(keyword1)
+
+if keyword1==("primary source"):
+        print("A primary sorce is direct proof or evidence of something that comes straight from that time period")
+elif keyword1==("secondary source"):
+        print("A secondary sorce is proof or evidence of something after that time perdiod")
+elif keyword1==("research archaeology"):
+        print("Research archaeology is using books, files and artifacts to find information about something")
+elif keyword1==("artifact"):
+        print("Artifacts are things from different times. Anything around you now could be an artifact and a few centuries")
+elif keyword1==("wattle and daub"):
+        print("Wattle and daub was a construction method, used by weaving branches around wooden poles and filled with mud and soil")
+elif keyword1==("mesolithic times"):
+        print("the mesolithic times started around 7000BC and ended around 5000BC")
+elif keyword1==("bronze age"):
+        print("The bronze age started around 2000BC. People started using bronze instead of stone for weapons and tools")
+elif keyword1==("monk"):
+        print("A monk is a holy man who lives in an abbey and devotes his life to God")
+elif keyword1==("abbot"):
+	print("The Abbot is the head monk in the monastary")
+elif keyword1==("plebian"):
+	print("A plebian was a poor person who usually lived in a small home and eat grains and cheap meats")
+elif keyword1==("patricians"):
+	print("Patricians were wealthy land owners sold land to both rich and poor people")
+elif keyword1==("all star"):
+        print("Somebody once told me the world is gonna roll me, I ain't the sharpest tool in the shed, She was looking kind of dumb with her finger and her thumb, In the shape of an L on her forehead, Well the years start coming and they don't stop coming, Fed to the rules and I hit the ground running, Didn't make sense not to live for fun, Your brain gets smart but your head gets dumb, So much to do, so much to see, So what's wrong with taking the back streets?, You'll never know if you don't go, You'll never shine if you don't glow, Hey now, your an Al STar, get your game on, go play")
+elif keyword1==("help"):
+	print("You can choose a definition for the following keywords: primary source, secondary source, research archaeology, wattle and daub, artifact, mesolithic times, bronze age, monk, abbot, plebian, patricians,")
+else:
+        print("I don't know that one")
+
+
+
+keyword1=input("Any more? ")
+
+print(keyword1)
+
+if keyword1==("primary source"):
+        print("A primary sorce is direct proof or evidence of something that comes straight from that time period")
+elif keyword1==("secondary source"):
+        print("A secondary sorce is proof or evidence of something after that time perdiod")
+elif keyword1==("research archaeology"):
+        print("Research archaeology is using books, files and artifacts to find information about something")
+elif keyword1==("artifact"):
+        print("Artifacts are things from different times. Anything around you now could be an artifact and a few centuries")
+elif keyword1==("wattle and daub"):
+        print("Wattle and daub was a construction method, used by weaving branches around wooden poles and filled with mud and soil")
+elif keyword1==("mesolithic times"):
+        print("the mesolithic times started around 7000BC and ended around 5000BC")
+elif keyword1==("bronze age"):
+        print("The bronze age started around 2000BC. People started using bronze instead of stone for weapons and tools")
+elif keyword1==("monk"):
+        print("A monk is a holy man who lives in an abbey and devotes his life to God")
+elif keyword1==("abbot"):
+	print("The Abbot is the head monk in the monastary")
+elif keyword1==("plebian"):
+	print("A plebian was a poor person who usually lived in a small home and eat grains and cheap meats")
+elif keyword1==("patricians"):
+	print("Patricians were wealthy land owners sold land to both rich and poor people")
+elif keyword1==("help"):
+	print("You can choose a definition for the following keywords: primary source, secondary source, research archaeology, wattle and daub, artifact, mesolithic times, bronze age, monk, abbot, plebian, patricians,")
+elif keyword1==("blue"):
+        print(" im blue dab a dee daba daa")
+else:
+        print("I don't know that one")
+
+print("Thats all, re-run the code to find more keywords")
